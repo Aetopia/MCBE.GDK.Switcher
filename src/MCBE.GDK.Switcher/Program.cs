d236be0625c3955fc753f4ded183fa3b3c9df376
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-11-13 23:02:08 +0530

    Basic error handling.

--- src/MCBE.GDK.Switcher/Program.cs
+++ src/MCBE.GDK.Switcher/Program.cs
@@ -1,9 +1,19 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 static class Program
 {
     static void Main()
     {
+        AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+        {
+            var exception = (Exception)args.ExceptionObject;
+            while (exception.InnerException is { }) exception = exception.InnerException;
+
+            MessageBox.Show($"{exception}", exception.GetType().Name, MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            Environment.Exit(1);
+        };
+
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(false);
         Application.SetUnhandledExceptionMode(UnhandledExceptionMode.ThrowException);
